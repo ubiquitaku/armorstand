b9f97702eb5faf41134f3713b1afae44c3483f78
ubiquitaku <ubiquitaku@outlook.jp>
2021-02-19 04:47:59 +0900

    多分完成

--- src/main/java/ubiquitaku/amasta/Amasta.java
+++ src/main/java/ubiquitaku/amasta/Amasta.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -51,8 +51,8 @@ public final class Amasta extends JavaPlugin implements Listener {
             }
             if (args[0].equals("set")) {
                 if (list.contains((Player) sender)) {
-                    sender.sendMessage("あんたハッククライアントかなんか使ってる？");
-                    return true;
+                    sender.sendMessage("インベ閉じただろ");
+                    list.remove((Player) sender);
                 }
                 Player p = (Player) sender;
                 list.add(p);
@@ -69,18 +69,29 @@ public final class Amasta extends JavaPlugin implements Listener {
                 ItemMeta m4 = s4.getItemMeta();
                 ItemStack s5 = new ItemStack(Material.SHIELD);
                 ItemMeta m5 = s5.getItemMeta();
+                ItemStack s6 = new ItemStack(Material.GREEN_STAINED_GLASS);
+                ItemMeta m6 = s6.getItemMeta();
                 m0.setDisplayName("あたま");
                 m1.setDisplayName("胴");
                 m2.setDisplayName("レギンス");
                 m3.setDisplayName("足");
                 m4.setDisplayName("右手");
                 m5.setDisplayName("左手");
+                m6.setDisplayName("確定");
+                s0.setItemMeta(m0);
+                s1.setItemMeta(m1);
+                s2.setItemMeta(m2);
+                s3.setItemMeta(m3);
+                s4.setItemMeta(m4);
+                s5.setItemMeta(m5);
+                s6.setItemMeta(m6);
                 inv.setItem(0,s0);
                 inv.setItem(1,s1);
                 inv.setItem(2,s2);
                 inv.setItem(3,s3);
                 inv.setItem(4,s4);
                 inv.setItem(5,s5);
+                inv.setItem(6,s6);
                 p.openInventory(inv);
             }
         }
@@ -88,14 +99,22 @@ public final class Amasta extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void click(InventoryCloseEvent e) {
+    public void click(InventoryClickEvent e) {
         if (list.size() == 0) {
             return;
         }
-        if (!list.contains(e.getPlayer())) {
+        if (!list.contains((Player) e.getWhoClicked())) {
             return;
         }
-        ArmorStand as = (ArmorStand) getServer().getWorld(e.getPlayer().getWorld().getName()).spawnEntity(e.getPlayer().getLocation(), EntityType.ARMOR_STAND);
+        try {
+            e.getCurrentItem().getItemMeta().getDisplayName().equals("確定");
+        } catch (NullPointerException exception) {
+            return;
+        }
+        if (!e.getCurrentItem().getItemMeta().getDisplayName().equals("確定")) {
+            return;
+        }
+        ArmorStand as = (ArmorStand) getServer().getWorld(e.getWhoClicked().getWorld().getName()).spawnEntity(e.getWhoClicked().getLocation(), EntityType.ARMOR_STAND);
         as.setInvisible(true);
         as.setItem(EquipmentSlot.HEAD,e.getInventory().getItem(0));
         as.setItem(EquipmentSlot.CHEST,e.getInventory().getItem(1));
@@ -103,6 +122,7 @@ public final class Amasta extends JavaPlugin implements Listener {
         as.setItem(EquipmentSlot.FEET,e.getInventory().getItem(3));
         as.setItem(EquipmentSlot.HAND,e.getInventory().getItem(4));
         as.setItem(EquipmentSlot.OFF_HAND,e.getInventory().getItem(5));
-        list.remove(e.getPlayer());
+        list.remove((Player) e.getWhoClicked());
+        list.remove((Player) e.getWhoClicked());
     }
 }
