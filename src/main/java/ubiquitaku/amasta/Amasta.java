4be7a0a445c985b886941fbc57750d9d552e2731
ubiquitaku <ubiquitaku@outlook.jp>
2021-02-19 18:04:46 +0900

    多分完成

--- src/main/java/ubiquitaku/amasta/Amasta.java
+++ src/main/java/ubiquitaku/amasta/Amasta.java
@@ -123,6 +123,5 @@ public final class Amasta extends JavaPlugin implements Listener {
         as.setItem(EquipmentSlot.HAND,e.getInventory().getItem(4));
         as.setItem(EquipmentSlot.OFF_HAND,e.getInventory().getItem(5));
         list.remove((Player) e.getWhoClicked());
-        list.remove((Player) e.getWhoClicked());
     }
 }
